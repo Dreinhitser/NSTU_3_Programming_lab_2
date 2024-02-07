2d668fcbd94eb065d0bb16472aa3c249bceaf379
Denis Rikhter <48100891+Dreinhitser@users.noreply.github.com>
2024-02-07 16:37:56 +0700

    Fixed codestyle

--- NSTU_3_Programming_lab_2/main.cpp
+++ NSTU_3_Programming_lab_2/main.cpp
@@ -8,45 +8,39 @@
 typedef std::vector<std::string> Strings;
 typedef std::map<char, Strings> Text;
 
-int main(int argc, char* argv[])
-{
+int main(int argc, char* argv[]) {
+	if (argc != 2) {
+		std::cerr << "Invalid number of arguments\n";
+		return 1;
+	}
+
 	std::ifstream in(argv[1]);
-	if (!in.is_open())
-	{
-		throw std::runtime_error("Failed to open input file!");
+	if (!in.is_open()) {
+		std::cerr << "Failed to open input file!\n";
 		return 1;
 	}
 
 	Text text;
-
 	std::string line = "";
 
 	while (getline(in, line))
-	{
 		text[line[0]].push_back(line);
-	}
 	in.close();
 
 	for (auto& it : text)
-	{
 		std::sort(it.second.begin(), it.second.end());
-	}
 
 	char symbol;
 	std::cout << "Enter symbol: ";
 	std::cin >> symbol;
 
-	if (text.find(symbol) == text.end())
-	{
+	if (text.find(symbol) == text.end()) {
 		std::cout << "There are no lines starting with this character\n";
 	}
-	else
-	{
+	else {
 		std::cout << "Strings start with " << symbol << ":\n";
 		for (auto& it : text[symbol])
-		{
 			std::cout << it << "\n";
-		}
 	}
 
 	return 0;
